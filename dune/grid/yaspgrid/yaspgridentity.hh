adedner/dune-grid

commit 33b2a7d8b957f1c7030572fa6821f1de5379fdfa

[yasp] Transform coordinates on periodic overlap

dune/grid/yaspgrid/yaspgridentity.hh
@@ -495,7 +495,26 @@ namespace Dune {
     //! geometry of this entity
     Geometry geometry () const {
       // the element geometry
-      GeometryImpl _geometry(_it.lowerleft(),_it.upperright());
+      auto ll = _it.lowerleft();
+      auto ur = _it.upperright();
+
+      // If on periodic overlap, transform coordinates by domain size
+      for (int i=0; i<dimworld; i++) {
+        if (gridlevel()->mg->isPeriodic(i)) {
+          int coord = transformingsubiterator().coord(i);
+          if (coord < 0) {
+            auto size = _g->mg->domainSize()[i];
+            ll[i] += size;
+            ur[i] += size;
+          } else if (coord + 1 > gridlevel()->mg->levelSize(gridlevel()->level(),i)) {
+            auto size = _g->mg->domainSize()[i];
+            ll[i] -= size;
+            ur[i] -= size;
+          }
+        }
+      }
+
+      GeometryImpl _geometry(ll,ur);
       return Geometry( _geometry );
     }
 

dune/grid/yaspgrid/yaspgridintersection.hh
@@ -224,16 +224,31 @@ namespace Dune {
       shift[_dir] = false;
 
       Dune::FieldVector<ctype,dimworld> ll, ur;
-
       for (int i=0; i<dimworld; i++)
       {
         int coord = _inside.transformingsubiterator().coord(i);
+
         if ((i == _dir) and (_face))
           coord++;
+
         ll[i] = _inside.transformingsubiterator().coordCont()->coordinate(i,coord);
         if (i != _dir)
           coord++;
         ur[i] = _inside.transformingsubiterator().coordCont()->coordinate(i,coord);
+
+        // If on periodic overlap, transform coordinates by domain size
+        if (_inside.gridlevel()->mg->isPeriodic(i)) {
+          int coord = _inside.transformingsubiterator().coord(i);
+          if (coord < 0) {
+            auto size = _inside.gridlevel()->mg->domainSize()[i];
+            ll[i] += size;
+            ur[i] += size;
+          } else if (coord + 1 > _inside.gridlevel()->mg->levelSize(_inside.gridlevel()->level(),i)) {
+            auto size = _inside.gridlevel()->mg->domainSize()[i];
+            ll[i] -= size;
+            ur[i] -= size;
+          }
+        }
       }
 
       GeometryImpl _is_global(ll,ur,shift);

dune/grid/yaspgrid.hh
@@ -743,7 +743,7 @@ namespace Dune {
               const YLoadBalance<dim>* lb = defaultLoadbalancer())
       : ccobj(comm), _torus(comm,tag,s,lb), leafIndexSet_(*this),
        _periodic(periodic), _coarseSize(s), _overlap(overlap),
-        keep_ovlp(true), adaptRefCount(0), adaptActive(false)
+        keep_ovlp(true), adaptRefCount(0), adaptActive(false), _L(L)
     {
       // check whether YaspGrid has been given the correct template parameter
       static_assert(is_same<Coordinates,EquidistantCoordinates<ctype,dim> >::value,
@@ -803,7 +803,7 @@ namespace Dune {
               const YLoadBalance<dim>* lb = defaultLoadbalancer())
       : ccobj(comm), _torus(comm,tag,s,lb), leafIndexSet_(*this),
        _periodic(periodic), _coarseSize(s), _overlap(overlap),
-        keep_ovlp(true), adaptRefCount(0), adaptActive(false)
+        keep_ovlp(true), adaptRefCount(0), adaptActive(false), _L(upperright - lowerleft)
     {
       // check whether YaspGrid has been given the correct template parameter
       static_assert(is_same<Coordinates,EquidistantOffsetCoordinates<ctype,dim> >::value,
@@ -875,8 +875,10 @@ namespace Dune {
       _levels.resize(1);
 
       //determine sizes of vector to correctly construct torus structure and store for later size requests
-      for (int i=0; i<dim; i++)
+      for (int i=0; i<dim; i++) {
         _coarseSize[i] = coords[i].size() - 1;
+        _L[i] = coords[i][_coarseSize[i]] - coords[i][0];
+      }
 
       iTupel o;
       std::fill(o.begin(), o.end(), 0);
@@ -962,7 +964,7 @@ namespace Dune {
               int overlap,
               const YLoadBalance<dim>* lb = defaultLoadbalancer())
       : ccobj(comm), _torus(comm,tag,s,lb), leafIndexSet_(*this),
-        keep_ovlp(true), adaptRefCount(0), adaptActive(false)
+        keep_ovlp(true), adaptRefCount(0), adaptActive(false), _L(L)
     {
       _periodic = periodic;
       _levels.resize(1);
@@ -1030,8 +1032,10 @@ namespace Dune {
       _overlap = overlap;
 
       //determine sizes of vector to correctly construct torus structure and store for later size requests
-      for (int i=0; i<dim; i++)
+      for (int i=0; i<dim; i++) {
         _coarseSize[i] = coords[i].size() - 1;
+        _L[i] = coords[i][_coarseSize[i]] - coords[i][0];
+      }
 
       iTupel o;
       std::fill(o.begin(), o.end(), 0);
@@ -1128,6 +1132,9 @@ namespace Dune {
 
       if (!Dune::Yasp::checkIfMonotonous(coords))
         DUNE_THROW(Dune::GridError,"Setup of a tensorproduct grid requires monotonous sequences of coordinates.");
+
+      for (int i=0; i<dim; i++)
+        _L[i] = coords[i][coords[i].size() - 1] - coords[i][0];
 
       _levels.resize(1);
 
@@ -1433,6 +1440,11 @@ namespace Dune {
     size_t numBoundarySegments () const
     {
       return nBSegments;
+    }
+
+    //! \brief returns the size of the physical domain
+    const Dune::FieldVector<ctype, dim>& domainSize () const {
+      return _L;
     }
 
     /*! The new communication interface
@@ -1832,7 +1844,7 @@ namespace Dune {
     YaspIndexSet<const YaspGrid<dim,Coordinates>, true> leafIndexSet_;
     YaspGlobalIdSet<const YaspGrid<dim,Coordinates> > theglobalidset;
 
-    fTupel _LL;
+    Dune::FieldVector<ctype, dim> _L;
     iTupel _s;
     std::bitset<dim> _periodic;
     iTupel _coarseSize;

commit 3a55d0422bd6b39eb0cb818aec0b73005d6d12a7

[YASP] partially revert 33b2a7d8
beside the storage of the globale domain size, 33b2a7d8 introduced a
breaking change in the YaspGrid geometry computations if working on
periodic grids.

There is no documentation about the precise semantics after the
change, it breaks code predating 2015 and it violates the documentate
behavior of YaspGrid parallel information. In the currentstate it is
really hard to have parallel periodic meshes, without special casing
the sequential case.

Actually this change should never have been merged.

Reverting this commit fixes #195

dune/grid/yaspgrid/yaspgridentity.hh
@@ -490,26 +490,7 @@ namespace Dune {
     //! geometry of this entity
     Geometry geometry () const {
       // the element geometry
-      auto ll = _it.lowerleft();
-      auto ur = _it.upperright();
-
-      // If on periodic overlap, transform coordinates by domain size
-      for (int i=0; i<dimworld; i++) {
-        if (gridlevel()->mg->isPeriodic(i)) {
-          int coord = transformingsubiterator().coord(i);
-          if (coord < 0) {
-            auto size = _g->mg->domainSize()[i];
-            ll[i] += size;
-            ur[i] += size;
-          } else if (coord + 1 > gridlevel()->mg->levelSize(gridlevel()->level(),i)) {
-            auto size = _g->mg->domainSize()[i];
-            ll[i] -= size;
-            ur[i] -= size;
-          }
-        }
-      }
-
-      GeometryImpl _geometry(ll,ur);
+      GeometryImpl _geometry(_it.lowerleft(),_it.upperright());
       return Geometry( _geometry );
     }
 

dune/grid/yaspgrid/yaspgridintersection.hh
@@ -248,20 +248,6 @@ namespace Dune {
         if (i != _dir)
           coord++;
         ur[i] = _inside.transformingsubiterator().coordCont()->coordinate(i,coord);
-
-        // If on periodic overlap, transform coordinates by domain size
-        if (_inside.gridlevel()->mg->isPeriodic(i)) {
-          int coordPeriodic = _inside.transformingsubiterator().coord(i);
-          if (coordPeriodic < 0) {
-            auto size = _inside.gridlevel()->mg->domainSize()[i];
-            ll[i] += size;
-            ur[i] += size;
-          } else if (coordPeriodic + 1 > _inside.gridlevel()->mg->levelSize(_inside.gridlevel()->level(),i)) {
-            auto size = _inside.gridlevel()->mg->domainSize()[i];
-            ll[i] -= size;
-            ur[i] -= size;
-          }
-        }
       }
 
       GeometryImpl _is_global(ll,ur,shift);